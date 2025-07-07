2dd42e573ec4d3f2a12774e495dc8216c5e36039
TheMayor001 <128826374+TheMayor001@users.noreply.github.com>
2025-07-07 19:24:14 +0300

    isolated html
    added css , js script files

---
+++ script.js
@@ -0,0 +1,14 @@
+const backToTopBtn = document.getElementById('back-to-top');
+const nav = document.getElementById('nav-menu');
+
+function toggleMenu() {
+  nav.classList.toggle('show');
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+});
